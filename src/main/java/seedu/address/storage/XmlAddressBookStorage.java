bac873037bb81f152976afa4477be343a6b3ef17
DESKTOP-6S5V5SO\conan <conantteo95@gmail.com>
2017-10-03 22:41:23 +0800

    FIxing overload methods declaration

--- src/main/java/seedu/address/storage/XmlAddressBookStorage.java
+++ src/main/java/seedu/address/storage/XmlAddressBookStorage.java
@@ -61,11 +61,6 @@ public class XmlAddressBookStorage implements AddressBookStorage {
         saveAddressBook(addressBook, filePath);
     }
 
-    @Override
-    public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
-        saveAddressBook(addressBook, getAddressBookFilePath() + "-backup.xml");
-    }
-
     /**
      * Similar to {@link #saveAddressBook(ReadOnlyAddressBook)}
      * @param filePath location of the data. Cannot be null
@@ -77,5 +72,10 @@ public class XmlAddressBookStorage implements AddressBookStorage {
         File file = new File(filePath);
         FileUtil.createIfMissing(file);
         XmlFileStorage.saveDataToFile(file, new XmlSerializableAddressBook(addressBook));
+    }
+
+    @Override
+    public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
+        saveAddressBook(addressBook, getAddressBookFilePath() + "-backup.xml");
     }
 }
